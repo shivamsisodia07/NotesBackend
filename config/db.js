b847daad39cf829b9dd195c2c191319e0d59ee5f
shivam sisodia <shivamsisodia8656816@gmail.com>
2023-06-29 23:34:26 +0530

    log of db connection added

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         const con = await mongoose.connect(process.env.MONGO_URI);
-        // console.log(`Mongo Connected: ${con.connection.host}`);
+        console.log(`Mongo Connected Successfully`);
     } catch (error) {
         console.log(error);
         process.exit(1);
